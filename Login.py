18fe11461b70b10e1b848a74c2c77fc5967c34e8
Said-Barokah <62392942+Said-Barokah@users.noreply.github.com>
2023-06-24 11:01:52 +0700

    Update Login.py

--- Login.py
+++ Login.py
@@ -46,7 +46,8 @@ else:
         data = pd.read_csv('data/data master.csv')
         test_size = pd.read_csv('data/meta/test_size.csv')
         X_train, X_test, y_train, y_test = train_test_split(data[['Nama']], data[['kelas']], test_size=test_size, random_state=1221)
-        st.sidebar.write(X_test.join(y_test))
+        st.sidebar.write(data)
+        #st.sidebar.write(X_test.join(y_test))
     if(demo_name == 'Klasifikasi'):
         st.sidebar.text('Parameter PSO')
         df_pso=pd.read_csv('data/meta/variabel_pso.csv').round(2)
